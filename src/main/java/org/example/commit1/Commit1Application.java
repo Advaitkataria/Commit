aa1d113719b86366cf7fac14c5930ec4246c273a
Advaitkataria <advait.katariaak@gmail.com>
2026-06-05 14:47:34 -0300

    Initial commit

---
+++ src/main/java/org/example/commit1/Commit1Application.java
@@ -0,0 +1,13 @@
+package org.example.commit1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Commit1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Commit1Application.class, args);
+    }
+
+}
