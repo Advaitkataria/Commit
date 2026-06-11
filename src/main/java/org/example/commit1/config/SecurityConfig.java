a7af3f561743ff99356ba8460bf27a8caaf391c0
Advaitkataria <advait.katariaak@gmail.com>
2026-06-11 12:12:48 -0300

    Updated CORS for Vercel

--- src/main/java/org/example/commit1/config/SecurityConfig.java
+++ src/main/java/org/example/commit1/config/SecurityConfig.java
@@ -90,7 +90,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://commit-frontend-l9bae2lew-advait-katarias-projects.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
