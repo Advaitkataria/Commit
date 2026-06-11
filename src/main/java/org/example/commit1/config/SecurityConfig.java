81b1b0eaa03677933e005a1a333ae802f7f0dd48
Advaitkataria <advait.katariaak@gmail.com>
2026-06-11 12:33:42 -0300

    Updated CORS for Vercel

--- src/main/java/org/example/commit1/config/SecurityConfig.java
+++ src/main/java/org/example/commit1/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.example.commit1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -45,6 +46,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/auth/admin-register").hasRole("ADMIN")
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
